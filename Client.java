64005be7890bf43a0edb7430dec545149395204c
Tanjila T <tanjilat@Tanjilas-MacBook-Air.local>
2025-04-12 20:43:29 -0400

    created base for client file with methods needed

--- Client.java
+++ Client.java
@@ -6,3 +6,29 @@ UML Diagram specifics:
 + handshake(): void
 + disconnect(): void
 */
+import java.util.*;
+
+
+public class Client {
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public Client (String address, int port) {
+
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void handshake() {
+
+    }
+
+    public void disconnect(){
+        
+    }
+
+
+}
